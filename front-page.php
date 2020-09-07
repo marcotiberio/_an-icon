b48a2ac731cf50669860e0117f5dcedbe932e615
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-07 15:16:09 +0200

    test

--- front-page.php
+++ front-page.php
@@ -512,16 +512,6 @@ get_header();
 
 </main><!-- #main -->
 
-<script>
-
-	var logo = document.getElementByClassName("custom-logo-link"),
-		
-	window.addEventListener("scroll", function() {
-		logo.style.transform = "rotate("+window.pageYOffset+"deg)";
-	});
-
-</script>
-
 <?php
 get_sidebar();
 get_footer();
