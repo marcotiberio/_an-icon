2311026299e59dbebe8472c7432d11812e739509
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-08 13:39:10 +0200

    test new rotation

--- js/script.js
+++ js/script.js
@@ -1,17 +1,60 @@
-$(function() {
-    var rotation = 0, 
-        scrollLoc = $(document).scrollTop();
-    $(window).scroll(function() {
-        var newLoc = $(document).scrollTop();
-        var diff = scrollLoc - newLoc;
-        rotation += diff, scrollLoc = newLoc;
-        var rotationStr = "rotate(" + rotation + "deg)";
-        $("#logo").css({
-            "-webkit-transform": rotationStr,
-            "-moz-transform": rotationStr,
-            "transform": rotationStr
-        });
-    });
-})
+// $(function() {
+//     var rotation = 0, 
+//         scrollLoc = $(document).scrollTop();
+//     $(window).scroll(function() {
+//         var newLoc = $(document).scrollTop();
+//         var diff = scrollLoc - newLoc;
+//         rotation += diff, scrollLoc = newLoc;
+//         var rotationStr = "rotate(" + rotation + "deg)";
+//         $("#logo").css({
+//             "-webkit-transform": rotationStr,
+//             "-moz-transform": rotationStr,
+//             "transform": rotationStr
+//         });
+//     });
+// })
+
+$('document').ready(function() {
+	
+	var lastScrollLeft = 0;
+
+	$(window).scroll(function trucenscroll(event) {
+
+	   var sl = $(this).scrollTop();
+	   var st = $(this).scrollLeft();
+
+	   if (sl > lastScrollLeft) {
+	       
+
+	    	var srotatey = "rotateY(" + sl/3 + "deg)";
+	    	var srotatex = "rotateX(" + sl/2 + "deg)";
+	    	var size = 100 + sl + "vw";
+
+			$("#logo").css({"-moz-transform" : srotatey + srotatex , "-webkit-transform" : srotatey + srotatex});
+
+
+
+	   }
+	   else if(sl == lastScrollLeft) {
+	     //do nothing 
+	     //In IE this is an important condition because there seems to be some instances where the last scrollTop is equal to the new one
+	   }
+	   else {
+
+	   		
+	    	var srotatey = "rotateY(" + sl/3 + "deg)";
+	    	var srotatex = "rotateX(" + sl/2 + "deg)";
+	    	var size = 100 + sl + "vw";
+			$("#logo").css({"-moz-transform" : srotatey + srotatex, "-webkit-transform" : srotatey + srotatex});
+
+
+
+	   }
+	   lastScrollLeft = sl;
+	});
+
+});
+
+
 
 
