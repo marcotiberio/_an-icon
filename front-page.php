903a5eed77f3b278b07ad65c53430bcd18bb0d7d
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-08 16:05:30 +0200

    test post title

--- front-page.php
+++ front-page.php
@@ -76,7 +76,16 @@ get_header();
 							?>
 							<article class="latestpost--archive" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 								<p>data</p>
-								<a href="<?php the_permalink(); ?>"><h2>titolo</h2></a>
+								<a href="<?php the_permalink(); ?>">
+									<h2>
+										<?php
+										if ( is_singular() ) :
+											the_title( '<h1 class="entry-title">', '</h1>' );
+										else :
+											the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+										endif;?>
+									</h2>
+								</a>
 								<p>autore</p>
 							</article>
 
