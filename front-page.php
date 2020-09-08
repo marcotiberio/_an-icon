01d28398fa994cc5b4a00012941f3f19eaf18b3f
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-08 13:09:48 +0200

    revert

--- front-page.php
+++ front-page.php
@@ -45,20 +45,20 @@ get_header();
 			<div class="left">
 				<div class="section-intro">
 					<?php the_field('seminars_intro'); ?>
-					<div class="archive">
-						<p>- Illusion 2019/2020</p>
-					</div>
+				</div>
+				<div class="archive">
+					<p>- Illusion 2019/2020</p>
 				</div>
 			</div>
-			<div class="post-list">
-				<h3 class="subsection-title">2019/2020</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-					labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-					laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-					voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-					non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			</div>
 			<div class="right">
+				<div class="post-list">
+					<h3 class="subsection-title">2019/2020</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+						labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+						laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+						voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+						non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
 				<div class="year-intro">
 					<h3 class="subsection-title">Illusion</h3>
 					<p>When dealing with environments that simulate reality, illusion is one of the first concepts that
