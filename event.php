4d835d11a78a2a9f371925aa93a3438e9bb258ce
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-11 15:17:22 +0200

    revert

--- event.php
+++ event.php
@@ -22,7 +22,6 @@ get_header();
 			the_post_navigation(
 				array(
 					'prev_text' => '<span class="nav-subtitle">' . esc_html__( '<', 'anicon' ) . '</span>',
-					the_time('j M Y')
 					'next_text' => '<span class="nav-subtitle">' . esc_html__( '>', 'anicon' ) . '</span>',
 				)
 			);
