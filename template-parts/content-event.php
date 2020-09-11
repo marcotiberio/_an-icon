d8242f957d455a90b5c2305f749abeced880daba
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-11 16:42:20 +0200

    test

--- template-parts/content-event.php
+++ template-parts/content-event.php
@@ -16,7 +16,40 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<div class="archive"></div>
+	<div class="archive">
+
+		<!-- Year Events -->
+		<?php 
+			$args = array(
+				'post_type' => 'post',
+				'post_status' => 'publish',
+				'category_name' => 'illusion',
+				'posts_per_page' => -1,
+			);
+			$arr_posts = new WP_Query( $args );
+				
+			if ( $arr_posts->have_posts() ) :
+				
+				
+				while ( $arr_posts->have_posts() ) :
+					$arr_posts->the_post();
+					?>
+					<article class="latestpost--archive" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<p><?php the_time('j M Y') ?></p>
+						<span><a href="<?php the_permalink(); ?>">
+							<?php
+							if ( is_singular() ) :
+								the_title( '<p class="entry-title">', '</p>' );
+							else :
+								the_title( '<p class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></p>' );
+							endif;?>
+						</a></span><span>autore</span>
+					</article>
+
+					<?php
+				endwhile;
+			endif; ?>
+	</div>
 
 	<?php anicon_post_thumbnail(); ?>
 
