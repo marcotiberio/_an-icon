6b42db7a9eed93873183a355c6394c15dce7049c
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-11 15:28:21 +0200

    revert

--- event.php
+++ event.php
@@ -24,63 +24,10 @@ get_header();
 				)
 			);
 
+			get_template_part( 'template-parts/content-event', get_post_type() );
+
 		endwhile; // End of the loop.
 		?>
-
-
-
-	<?php anicon_post_thumbnail(); ?>
-
-<div class="entry-content">
-	<header class="entry-header">
-		<h1>Event</h1>
-	</header>
-	<div class="entry-info">
-		<p><?php the_time('j M Y') ?></p>
-		<p>17:00 – 19:00</p>
-	</div>
-	<div class="event-type">
-		<h1>Seminar of Philosophy of Image</h1>
-	</div>
-	<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-
-		if ( 'post' === get_post_type() ) :
-			?>
-	<?php endif; ?>
-	<?php
-	the_content(
-		sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'anicon' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			wp_kses_post( get_the_title() )
-		)
-	);
-
-	wp_link_pages(
-		array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'anicon' ),
-			'after'  => '</div>',
-		)
-	);
-	?>
-</div><!-- .entry-content -->
-
-<footer class="entry-footer">
-	<?php anicon_entry_footer(); ?>
-</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
 
 	</main><!-- #main -->
 
