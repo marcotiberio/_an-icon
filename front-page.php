577b1f50ab81e43a072e60539593fa4f8a5ee5af
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-14 18:08:56 +0200

    test

--- front-page.php
+++ front-page.php
@@ -264,6 +264,20 @@ get_header();
 		</article>
 	</section>
 
+	<section id="publications-test-test">
+		<?php 
+		if ( have_posts() ) : 
+			while ( have_posts() ) : the_post(); 
+				the_title( '<h2>', '</h2>' ); 
+				the_post_thumbnail(); 
+				the_excerpt();
+			endwhile; 
+		else: 
+			_e( 'Sorry, no posts matched your criteria.', 'textdomain' ); 
+		endif; 
+		?>
+	</section>
+
 	<!-- <section id="publications">
 		<div class="left"></div>
 		<div class="right">
