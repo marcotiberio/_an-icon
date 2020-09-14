d71e108a8a4bd8d4eb474b6b7b3c536dcae27e47
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-14 14:41:06 +0200

    test

--- front-page.php
+++ front-page.php
@@ -244,59 +244,16 @@ get_header();
 		<div class="left"></div>
 		<div class="right">
 			<div class="top">
-			<div class="entry-content">
-		<div class="entry-info">
-			<div class="event-nav">
-			<?php
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'anicon' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'anicon' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
-			?>
-			</div>
-			<div class="event-time">
-				<p>17:00 – 19:00</p>
-			</div>
-		</div>
-		<div class="event-type">
-			<h1>Seminar of Philosophy of Image</h1>
-		</div>
-		<?php
-			if ( is_singular() ) :
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			else :
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			endif;
-
-			if ( 'post' === get_post_type() ) :
+				<?php 
+				if ( have_posts() ) {
+					while ( have_posts() ) {
+						the_post(); 
+						//
+						// Post Content here
+						//
+					} // end while
+				} // end if
 				?>
-		<?php endif; ?>
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'anicon' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'anicon' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
 			</div>
 			<div class="bottom"></div>
 		</div>
