8bead08db43443fd710bddec679daa42a5b9b0b7
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-14 10:05:48 +0200

    test sidebar 2

--- functions.php
+++ functions.php
@@ -133,6 +133,18 @@ function anicon_widgets_init() {
 			'after_title'   => '</h2>',
 		)
 	);
+
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Custom Sidebar', 'anicon' ),
+			'id'            => 'sidebar-2',
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+
 }
 add_action( 'widgets_init', 'anicon_widgets_init' );
 
--- sidebar2.php
+++ sidebar2.php
@@ -7,11 +7,11 @@
  * @package anicon
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+if ( ! is_active_sidebar( 'sidebar-2' ) ) {
 	return;
 }
 ?>
 
 <aside id="secondary" class="widget-area">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	<p>Test</p>
 </aside><!-- #secondary -->
