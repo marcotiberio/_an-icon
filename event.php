262ea0e37311d366c3731499015ef69d906c9c43
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-18 09:34:54 +0200

    test

--- event.php
+++ event.php
@@ -21,8 +21,17 @@ get_header();
 
 			get_template_part( 'template-parts/content-event', get_post_type() );
 
+			the_post_navigation(
+				array(
+					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'anicon' ) . '</span> <span class="nav-title">%title</span>',
+					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'anicon' ) . '</span> <span class="nav-title">%title</span>',
+				)
+			);
+
 		endwhile; // End of the loop.
 		?>
+
+		
 
 	</main><!-- #main -->
 
