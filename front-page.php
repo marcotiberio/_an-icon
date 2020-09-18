e719c408c674dd2bfb79f68ad1928c1955099014
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-18 12:30:58 +0200

    test

--- front-page.php
+++ front-page.php
@@ -69,7 +69,7 @@ get_header();
 								//Check if all day, set format accordingly
 								$format = ( eo_is_all_day($event->ID) ? get_option('date_format') : get_option('date_format') );
 								printf(
-									'<li><a href="%s"> %s </a> %s </li>',
+									'<li><a href="%s"> %s </a> %s <?php the_field("author"); ?> </li> ',
 									get_permalink($event->ID),
 									get_the_title($event->ID),
 									eo_get_the_start( $format, $event->ID, $event->occurrence_id )
