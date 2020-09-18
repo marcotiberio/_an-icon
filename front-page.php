41c81d3075fc4c0b3eb03e046582e87b7475b8e2
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-18 13:01:16 +0200

    test

--- front-page.php
+++ front-page.php
@@ -213,25 +213,7 @@ get_header();
 	<section id="events">
 		<div id="event-list">
 			<?php
-				$events = eo_get_events(array(
-						'numberposts'=>10,
-						'showpastevents'=>true,//Will be deprecated, but set it to true to play it safe.
-					));
-
-				if($events):
-					echo '<ul>';
-					foreach ($events as $event):
-						//Check if all day, set format accordingly
-						$format = ( eo_is_all_day($event->ID) ? get_option('date_format') : get_option('date_format') );
-						printf(
-							'<li><a href="%s"> %s </a> %s </li>',
-							get_permalink($event->ID),
-							get_the_title($event->ID),
-							eo_get_the_start( $format, $event->ID, $event->occurrence_id )
-						);
-					endforeach;
-					echo '</ul>';
-				endif;
+				get_template_part( 'event-organiser/eo-loop-single-event.php');
 			?>
 		</div>
 		<div id="customSidebar" class="sidebar">
