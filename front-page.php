5bcbf8945df40cbfb2e0380d37f6f22e3b6dcced
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-18 09:29:07 +0200

    revert

--- front-page.php
+++ front-page.php
@@ -242,28 +242,7 @@ get_header();
 
 	<section id="events">
 		<div>
-		<?php
-			$events = eo_get_events(array(
-				'numberposts'=>5,
-				'event_start_after'=>'today',
-				'showpastevents'=>true,//Will be deprecated, but set it to true to play it safe.
-			));
-
-			if($events):
-				echo '<ul>';
-				foreach ($events as $event):
-					//Check if all day, set format accordingly
-					$format = ( eo_is_all_day($event->ID) ? get_option('date_format') : get_option('date_format').' '.get_option('time_format') );
-					printf(
-							'<li><a href="%s"> %s </a> on %s </li>',
-							get_permalink($event->ID),
-							get_the_title($event->ID),
-							eo_get_the_start($format, $event->ID,null,$event->occurrence_id)
-					);
-				endforeach;
-				echo '</ul>';
-			endif;
-		?>
+			
 		</div>
 		<div id="customSidebar" class="sidebar">
 			<?php dynamic_sidebar( 'custom-sidebar' ); ?>
