a7aa5ea7af7e7ca7073f55cb6c900c5cb2988e05
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-18 11:51:43 +0200

    test

--- front-page.php
+++ front-page.php
@@ -59,7 +59,7 @@ get_header();
 						<ul>
 						<?php
 							$recent_posts = wp_get_recent_posts(array('post_type'=>'event',
-											'include' => get_cat_ID($atts['category'])));
+											'include' => get_cat_ID($atts['illusion'])));
 							foreach( $recent_posts as $recent ){
 								echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
 							}
