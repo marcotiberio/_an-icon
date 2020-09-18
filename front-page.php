43e33880d0c15d7e1190f2480a066d911a62747e
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-18 09:46:57 +0200

    test

--- front-page.php
+++ front-page.php
@@ -242,7 +242,32 @@ get_header();
 
 	<section id="events">
 		<div>
-			
+			<?php if ( have_posts() ) { ?>
+
+				<?php eo_get_template_part( 'eo-events-nav' ); //Events navigation ?>
+
+				<?php
+				while ( have_posts() ) : the_post();
+					eo_get_template_part( 'eo-loop-single-event' );
+				endwhile;
+				?>
+
+				<?php eo_get_template_part( 'eo-events-nav' ); //Events navigation ?>
+
+				<?php } else { ?>
+
+				<!-- If there are no events -->
+				<article id="post-0" class="post no-results not-found">
+					<header class="entry-header">
+						<h1 class="entry-title"><?php _e( 'Nothing Found', 'eventorganiser' ); ?></h1>
+					</header><!-- .entry-header -->
+
+					<div class="entry-content">
+						<p><?php _e( 'Apologies, but no results were found for the requested archive. ', 'eventorganiser' ); ?></p>
+					</div><!-- .entry-content -->
+				</article><!-- #post-0 -->
+
+			<?php };?>
 		</div>
 		<div id="customSidebar" class="sidebar">
 			<?php dynamic_sidebar( 'custom-sidebar' ); ?>
