4c026402d5d6fd28509a7c008e27a268873ef415
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-18 18:18:15 +0200

    test

--- front-page.php
+++ front-page.php
@@ -210,7 +210,18 @@ get_header();
 
 	<section id="events">
 		<div id="event-list">
-			
+		<main id="primary" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content-event', get_post_type() );
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
 		</div>
 		<div id="customSidebar" class="sidebar">
 			<?php dynamic_sidebar( 'custom-sidebar' ); ?>
