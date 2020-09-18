357969086e75d7b63255c792258948cd70d38394
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-18 12:54:24 +0200

    test

--- event-organiser/single-event.php
+++ event-organiser/single-event.php
@@ -59,7 +59,7 @@ get_header(); ?>
 						the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 					endif;
 
-					if ( 'post' === get_post_type() ) :
+					if ( 'event' === get_post_type() ) :
 						?>
 				<?php endif; ?>
 				<?php
